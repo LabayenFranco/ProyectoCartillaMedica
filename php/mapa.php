5bc26ac85fa20fc029f359fb0db31cca29c012a8
Franco <83313551+LabayenFranco@users.noreply.github.com>
2021-11-09 20:20:00 -0300

    Add files via upload

---
+++ php/mapa.php
@@ -0,0 +1,17 @@
+<?php
+    include 'conex.php';
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Hospitales Cercanos</title>
+		<link rel="stylesheet" href="../css/mapa.css">
+	</head>
+	<body>
+	    <div class="contein">
+	        <iframe src="https://www.google.com/maps/d/embed?mid=1vQjHVwCvs8l1_OE6Ji66D-DXB4HDMV7L"></iframe>
+	    </div>
+	</body>
+</html>
